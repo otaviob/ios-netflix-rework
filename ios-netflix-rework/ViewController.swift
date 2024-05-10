bd30109ad00e7e20188178127d4c08ad04bb3dee
otaviob <otaviob22@gmail.com>
2024-05-10 21:27:52 +1000

    Initial Commit

---
+++ ios-netflix-rework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-netflix-rework
+//
+//  Created by Otavio Brito on 10/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
